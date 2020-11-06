d9d2d82d283cf4ebcf956cc1d54e4c75dcdcd453
Cockhash <74072409+Cockhash@users.noreply.github.com>
2020-11-06 20:40:07 +0100

    Update 0_installation.sh

--- 0_installation.sh
+++ 0_installation.sh
@@ -10,7 +10,7 @@
 
 # ATTENTION
 #--------------------------------------------------
-# Created by XaiMloop
+# Created by Cockhash
 #--------------------------------------------------
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by

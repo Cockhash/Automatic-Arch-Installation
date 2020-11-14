9c623b0085aa14e51342c1e8c39d4efbf166daa9
Cockhash <74072409+Cockhash@users.noreply.github.com>
2020-11-14 22:37:18 +0100

    Update 0_installation.sh

--- 0_installation.sh
+++ 0_installation.sh
@@ -165,12 +165,13 @@ echo "--------------------------------------"
 echo "--    Arch Install on Main Drive    --"
 echo "--------------------------------------"
 
-pacstrap -i /mnt base base-devel os-prober efibootmgr grub linux linux-firmware linux-headers vim nano sudo lvm2 cryptdevice --noconfirm --needed
+pacstrap /mnt base base-devel os-prober efibootmgr grub linux linux-firmware linux-headers vim nano sudo lvm2 cryptdevice --noconfirm --needed
 
 echo "--------------------------------------"
 echo "--    Set-up Internet connection    --"
 echo "--------------------------------------"
-pacstrap -i /mnt net-tools networkmanager network-manager-applet netctl wireless_tools wpa_supplicant dialog --noconfirm --needed
+
+pacstrap /mnt net-tools networkmanager network-manager-applet netctl wireless_tools wpa_supplicant dialog --noconfirm --needed
 
 arch-chroot /mnt /bin/bash <<"CHROOT"
 

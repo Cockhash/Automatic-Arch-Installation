5211948242ee7c50c6172933fc0f0e928f36dfd9
Cockhash <74072409+Cockhash@users.noreply.github.com>
2021-03-15 20:38:02 +0100

    Add files via upload

---
+++ archmatic.sh
@@ -0,0 +1,515 @@
+#!/usr/bin/env sh
+
+#-------------------------------------------------------------------------
+#   Author: uououo 
+#   Last Edit: 14.03.2021
+#   Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+function preinstall {
+    # UEFI / BIOS detection
+    efivar -l >/dev/null 2>&1
+    
+    if [[ $? -eq 0 ]]; then
+        echo "UEFI detected. Installation will go on."
+    else
+        echo ="BIOS detected. This Installation Script is for UEFI only! Execution aborted."
+        exit
+    fi
+
+    # check ethernet connection
+    ping -c3 archlinux.org
+
+    if [ $? -eq 0 ]; then
+        echo "Internet connection detected. Installation will go on."
+    else
+        echo ="Internet connection failed. Execution aborted."
+        exit
+    fi
+
+    # update repos
+    pacman -Sy
+
+    # sync systemclock
+    timedatectl set-ntp true
+
+    # Prompts
+
+    lsblk
+
+    echo -e "\nEnter your drive: /dev/sda or /dev/nvme0n1"
+    read disk
+
+    if [ "$disk" == "/dev/nvme0n1" ]; then
+        disk_boot="${disk}p1"
+        disk_esp="${disk}p2"
+        disk_lvm="${disk}p3"
+        disk_lvm_sed="\/dev\/nvme0n1p3"
+    fi
+
+    if [ "$disk" == "/dev/nvme0n2" ]; then
+        disk_boot="${disk}p1"
+        disk_esp="${disk}p2"
+        disk_lvm="${disk}p3"
+        disk_lvm_sed="\/dev\/nvme0n2p3"
+    fi
+
+    if [ "$disk" == "/dev/nvme0n3" ]; then
+        disk_boot="${disk}p1"
+        disk_esp="${disk}p2"
+        disk_lvm="${disk}p3"
+        disk_lvm_sed="\/dev\/nvme0n3p3"
+    fi
+
+    if [ "$disk" == "/dev/sda" ]; then
+        disk_boot="${disk}1"
+        disk_esp="${disk}2"
+        disk_lvm="${disk}3"
+        disk_lvm_sed="\/dev\/sda3"
+    fi
+
+    if [ "$disk" == "/dev/sdb" ]; then
+        disk_boot="${disk}1"
+        disk_esp="${disk}2"
+        disk_lvm="${disk}3"
+        disk_lvm_sed="\/dev\/sdb3"
+    fi
+
+    if [ "$disk" == "/dev/sdc" ]; then
+        disk_boot="${disk}1"
+        disk_esp="${disk}2"
+        disk_lvm="${disk}3"
+        disk_lvm_sed="\/dev\/sdc3"
+    fi
+
+    echo -e "\nEnter hostname:"
+    read hostname
+
+    echo -e "\nEnter ROOT password:"
+    read -s root_password
+
+    echo -e "\nVerify the ROOT password:"
+    read -s root_password2
+
+    # Check if both passwords match
+    if [ "$root_password" != "$root_password2" ]; then
+        echo "Passwords do not match!"
+        exit
+    fi
+
+    echo -e "\nEnter username:"
+    read user
+
+    echo -e "\nEnter USER password:"
+    read -s user_password
+
+    echo -e "\nVerify USER password:"
+    read -s user_password2
+
+    # Check if both passwords match
+    if [ "$user_password" != "$user_password2" ]; then
+        echo "Passwords do not match!"
+        exit
+    fi
+
+    echo -e "\nSelect Distro Environment: [gnome/kde]"
+    read de
+
+    echo -e "\nDo you want a swapfile? [yes/no]"
+    read swap
+
+    echo -e "\nDo you want the Laptop packages (wifi, bluetoth, etc.)? [yes/no]"
+    read ucode
+
+    echo -e "\nWhat CPU do you have? [intel/amd]"
+    read laptop
+
+    # export environment variabels
+    export disk
+    export disk_boot
+    export disk_esp
+    export disk_lvm
+    export disk_lvm_sed
+    export hostname
+    export root_password
+    export user
+    export user_password
+    export de
+    export swap
+    export ucode
+    export laptop
+}
+
+function baseInstall {
+    # Format disk
+
+    # Disk prep
+    sgdisk -Z $disk             # zap all on disk
+    sgdisk -a 2048 -o $disk     # new gpt disk 2048 alignment
+
+    # Create partition layout
+    sgdisk -n 1:0:+1024M $disk  # partition 1 (boot), default start block, size: 1024MB
+    sgdisk -n 2:0:+1024M $disk  # partition 2 (esp), default start block, size: 1024MB
+    sgdisk -n 3:0:0 $disk       # partition 3 (lvm), default start, size: remaining space
+
+    # Set partition types
+    sgdisk -t 1:8300 $disk
+    sgdisk -t 2:ef00 $disk
+    sgdisk -t 3:8e00 $disk
+
+    # Label partitions
+    sgdisk -c 1:"boot" $disk
+    sgdisk -c 2:"esp" $disk
+    sgdisk -c 3:"lvm" $disk
+
+    # Create LUKS encrypted lvm partition
+    cryptsetup luksFormat -c aes-xts-plain -y -s 512 -h sha512 $disk_lvm
+    cryptsetup luksOpen $disk_lvm lvm
+
+    # Set-up lvm
+    pvcreate /dev/mapper/lvm
+    vgcreate main /dev/mapper/lvm
+    lvcreate -l 100%FREE -n lv_root main
+
+    modprobe dm-crypt
+    vgscan
+    vgchange -ay
+
+    # Create filesystem
+    mkfs.ext4 $disk_boot
+    mkfs.fat -F32 $disk_esp
+    mkfs.ext4 /dev/main/lv_root
+
+    # Mount target
+    mount /dev/main/lv_root /mnt
+    mkdir /mnt/boot
+    mount $disk_boot /mnt/boot
+    mkdir /mnt/boot/esp
+    mount $disk_esp /mnt/boot/esp
+
+    # Generate /etc/fstab
+    mkdir /mnt/etc
+    genfstab -Up /mnt >> /mnt/etc/fstab
+
+    # Install base
+    pacstrap /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr os-prober dosfstools mtools lvm2 --noconfirm --needed
+
+    # Install basic Networking tools
+    pacstrap /mnt networkmanager netctl --noconfirm --needed
+    systemctl enable NetworkManager
+
+    arch-chroot /mnt /bin/bash <<"CHROOT"
+    
+        # Set root password
+        echo "root:${root_password}" | chpasswd
+
+        ### Bootloader
+        ## Reduce grub delay on boot
+        sed -i -e 's/GRUB_TIMEOUT=3/GRUB_TIMEOUT=0/g' /etc/default/grub
+
+        # $disk_lvm_sed is needed because of the slashes of $disk_lvm, that need to be escaped
+        sed -i -e "s/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=${disk_lvm_sed}:main:allow-discards loglevel=3\"/g" /etc/default/grub
+
+        # Enable encrypted boot
+        sed -i -e 's/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/g' /etc/default/grub
+
+        # Install grub
+        grub-install --target=x86_64-efi --efi-directory=/boot/esp --bootloader-id=grub_uefi --recheck --debug
+        # Copy the locale file to locale directory
+        cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+        # Generate GRUB's config file
+        grub-mkconfig -o /boot/grub/grub.cfg
+
+        # Update initial ramdisk
+        sed -i -e 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck)/g' /etc/mkinitcpio.conf
+        mkinitcpio -p linux
+CHROOT
+}
+
+function baseSetup {
+    arch-chroot /mnt /bin/bash <<"CHROOT"
+
+        # Set locales
+        echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+        locale-gen
+        echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+        # Set time zone
+        ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+
+        # Specify cores for simultaneous compiling
+        sudo sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$(nproc)\"/g" /etc/makepkg.conf
+        # Change compression settings for "$nproc" cores.
+        sudo sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $(nproc) -z -)/g" /etc/makepkg.conf
+
+        # Set hostname
+        echo $hostname > /etc/hostname
+
+        # Configure hosts file
+        echo "127.0.1.1 $hostname" >> /etc/hosts
+
+        # Set-up user account
+        useradd -m -g users -G wheel $user
+        echo "${user}:${user_password}" | chpasswd
+
+        # Enable sudo-privileges for group "wheel"
+        sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+
+        # Set-up 8GB swapfile
+        if [ "$swap" == "yes" ]; then
+            dd if=/dev/zero of=/swapfile bs=1M count=8192 status=progress
+            chmod 600 /swapfile
+            mkswap /swapfile
+            swapon /swapfile
+            echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+        fi
+
+        # Install CPU Microcode files
+        pacman -S ${ucode}-ucode --noconfirm --needed
+CHROOT
+}
+
+
+function softwareDesk {
+    arch-chroot /mnt /bin/bash <<"CHROOT"
+
+        ### Pacman packages
+    
+        PKGS=(
+            # XORG DISPLAY RENDERING -------------------------------------------------------------
+            'xorg'                      # Base Package
+            'xorg-drivers'              # Display Drivers
+            'xorg-server'               # Xorg server
+            'xorg-xinit'                # Xorg init
+            'xorg-xinput'               # Xorg input
+            'mesa'                      # Open source version of OpenGL
+
+            # NETWORK SETUP ----------------------------------------------------------------------
+            'openvpn'                   # Open VPN support
+            'networkmanager-openvpn'    # Open VPN plugin for NM
+            'network-manager-applet'    # System tray icon/utility for network connectivity
+
+            # AUDIO ------------------------------------------------------------------------------
+            'alsa-utils'                # Advanced Linux Sound Architecture (ALSA) Components https://alsa.opensrc.org/
+            'alsa-plugins'              # ALSA plugins
+            'pulseaudio'                # Pulse Audio sound components
+            'pulseaudio-alsa'           # ALSA configuration for pulse audio
+            'pavucontrol'               # Pulse Audio volume control
+            'pnmixer'                   # System tray volume control
+
+            # PRINTERS ---------------------------------------------------------------------------
+            'cups'                      # Open source printer drivers
+            'cups-pdf'                  # PDF support for cups
+            'ghostscript'               # PostScript interpreter
+            'gsfonts'                   # Adobe Postscript replacement fonts
+            'hplip'                     # HP Drivers
+            'system-config-printer'     # Printer setup  utility
+
+            # TERMINAL UTILITIES -----------------------------------------------------------------
+            'cronie'                    # cron jobs
+            'curl'                      # Remote content retrieval
+            'wget'                      # Remote content retrieval
+            'htop'                      # Process viewer
+            'hardinfo'                  # Hardware info app
+            'neofetch'                  # Shows system info when you launch terminal
+            'ntp'                       # Network Time Protocol to set time via network.
+            'numlockx'                  # Turns on numlock in X11
+            'openssh'                   # SSH connectivity tools
+            'p7zip'                     # 7z compression program
+            'rsync'                     # Remote file sync utility
+            'speedtest-cli'             # Internet speed via terminal
+            'terminus-font'             # Font package with some bigger fonts for login terminal
+            'unrar'                     # RAR compression program
+            'unzip'                     # Zip compression program
+            'vim'                       # Text Editor
+            'nano'                      # Text Editor
+            'reflector'                 # Tool for fetching latest mirrors
+
+            # DISK UTILITIES ---------------------------------------------------------------------
+            'android-tools'             # ADB for Android
+            'autofs'                    # Auto-mounter
+            'dosfstools'                # DOS Support
+            'exfat-utils'               # Mount exFat drives
+            'filezilla'                 # SSH File Transfer
+
+            # GENERAL UTILITIES ------------------------------------------------------------------
+            'freerdp'                   # RDP Connections
+            'libvncserver'              # VNC Connections
+            'remmina'                   # Remote Connection
+            'veracrypt'                 # Disc encryption utility
+            'keepassxc'                 # Password Manager
+
+            # DEVELOPMENT ------------------------------------------------------------------------
+            'clang'                     # C Lang compiler
+            'cmake'                     # Cross-platform open-source make system
+            'electron'                  # Cross-platform development using Javascript
+            'git'                       # Version control system
+            'gcc'                       # C/C++ compiler
+            'glibc'                     # C libraries
+            'meld'                      # File/directory comparison
+            'nodejs'                    # Javascript runtime environment
+            'npm'                       # Node package manager
+            'python'                    # Scripting language
+            'yarn'                      # Dependency management (Hyper needs this)
+            'go'                        # Import programming language
+
+            # MEDIA ------------------------------------------------------------------------------
+            'obs-studio'                # Record your screen
+            'vnc'                       # Video player
+
+            # GRAPHICS AND DESIGN ----------------------------------------------------------------
+            'gimp'                      # GNU Image Manipulation Program
+
+            # BROWSER ----------------------------------------------------------------------------
+            'firefox'                   # Browser
+            'torbrowser-launcher'       # Onion Routing
+
+            # COMMUNICATION ----------------------------------------------------------------------
+            'thunderbird'               # Mail Client
+            'element-desktop'           # Matrix Client for Communication
+
+            # OFFICE -----------------------------------------------------------------------------
+            'libreoffice'               # Office Suite
+        )
+        for PKG in "${PKGS[@]}"; do
+        sudo pacman -S ${PKG} --noconfirm --needed
+        done
+        
+        
+        ### AUR setup
+        
+        # Add sudo no-password privileges
+        sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+
+        su $user
+
+        # Install paru (AUR Helper)
+        cd /tmp
+        git clone "https://aur.archlinux.org/paru.git" && cd paru
+        makepkg -sric --noconfirm && cd
+
+        PKGS=(
+            # UTILITIES --------------------------------------------------------------------------
+            'timeshift'                 # Backup programm
+            'brave-bin'                 # Alternative chrome-based browser
+        )
+        for PKG in "${PKGS[@]}"; do
+        paru -Syu --noconfirm $PKG
+        done
+
+        # Remove sudo no-password privileges
+        sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+        
+        
+        ### Desktop Environment (Plasma and Gnome)
+        
+        if [ "$de" == "kde" ]; then
+            PKGS=(
+                # Plasma -----------------------------------------------------------------------------
+                'plasma-meta'               # Desktop Environment
+                'kde-applications-meta'     # KDE Applications
+                'packagekit-qt5'            # Discover Back-end for standard arch repos
+                'xdg-user-dirs'             # Create user directories in Dolphin
+                'sddm'                      # Login Manager
+            )
+            for PKG in "${PKGS[@]}"; do
+            pacman -Syu --noconfirm $PKG
+            done
+
+            PKGS=(
+                # Remove unnecessary packages, that came as dependencies
+                'kde-education-meta'
+                'kde-games-meta'
+                'kde-multimedia-meta'
+            )
+            for PKG in "${PKGS[@]}"; do
+            pacman -R ${PKG} --noconfirm --needed
+            done
+        fi
+
+        if [ "$de" == "gnome" ]; then
+            PKGS=(
+                # GNOME --------------------------------------------------------------------------------
+                'gnome'                     # Desktop Environment
+                'gnome-tweaks'              # GNOME tuning tool
+                'gdm'                       # Login Manager
+            )
+            for PKG in "${PKGS[@]}"; do
+            pacman -Syu --noconfirm $PKG
+            done
+        fi
+    
+    
+        ### Laptop packages
+        
+        if [ "$laptop" == "yes" ]; then
+            PKGS=(
+                # WIRELESS ---------------------------------------------------------------------------
+                'dialog'                    # Enables shell scripts to trigger dialog boxex
+                'wpa_supplicant'            # Key negotiation for WPA wireless networks
+                'wireless_tools'            # wireless tools
+
+                # BLUETOOTH --------------------------------------------------------------------------
+                'bluez'                     # Daemons for the bluetooth protocol stack
+                'bluez-utils'               # Bluetooth development and debugging utilities
+                'bluez-firmware'            # Firmwares for Broadcom BCM203x and STLC2300 Bluetooth chips
+                'blueberry'                 # Bluetooth configuration tool
+                'pulseaudio-bluetooth'      # Bluetooth support for PulseAudio
+
+                # OTHERS -----------------------------------------------------------------------------
+                'tlp'                       # Advanced laptop power management
+            )
+            for PKG in "${PKGS[@]}"; do
+            pacman -Syu --noconfirm $PKG
+            done
+        fi
+CHROOT
+}
+
+function final {
+    arch-chroot /mnt /bin/bash <<"CHROOT"
+
+        # Enable Login Manager
+        if [ "$de" == "kde" ]; then
+            systemctl enable sddm
+        fi
+
+        if [ "$de" == "gnome" ]; then
+            systemctl enable gdm
+        fi
+
+        # Enable bluetooth daemon and setting it to auto-start
+        sed -i 's|#AutoEnable=false|AutoEnable=true|g' /etc/bluetooth/main.conf
+        systemctl enable bluetooth
+
+        # Enablie cups service daemon so we can print
+        systemctl enable cups
+
+        ### Set-up ZSH
+        # Change shell
+        sudo chsh -s /bin/zsh $(whoami)
+        touch "~/.cache/zshhistory"
+        # Fetch zsh config
+        wget https://raw.githubusercontent.com/Cockhash/zsh/main/.zshrc -O ~/.zshrc
+        mkdir -p "~/.zsh"
+        # Setup Alias for powerlevel10k in ~/zsh/aliasrc
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+        # Install awesome terminl font from https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+        
+        # Clean orphans pkg
+        if [[ ! -n $(pacman -Qdt) ]]; then
+            echo "No orphans to remove."
+        else
+            pacman -Rns $(pacman -Qdtq)
+        fi
+CHROOT
+}
+
+preinstall
+baseInstall
+baseSetup
+softwareDesk
+final
+
+echo "\nThe installation has finished."
+exit

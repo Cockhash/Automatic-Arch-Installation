63f495e7657dcd0ee090b11b026a281c8390b918
Cockhash <74072409+Cockhash@users.noreply.github.com>
2021-04-15 18:13:35 +0200

    Update archmatic.sh

--- archmatic.sh
+++ archmatic.sh
@@ -289,6 +289,10 @@ function softwareDesk {
             'xorg-server'               # Xorg server
             'xorg-xinit'                # Xorg init
             'xorg-xinput'               # Xorg input
+            'xorg-xwayland'             # Temp: Wayland Support
+            'xorg-xeyes'                # Temp: Wayland Support
+            'qt5-wayland'               # Temp: Wayland Support
+            'glfw-wayland'              # Temp: Wayland Support
             'mesa'                      # Open source version of OpenGL
 
             # NETWORK SETUP ----------------------------------------------------------------------
